009f4b3414ca72170ca1be08bc2749f8ccba3660
Claudio dAppollonio <claudio.d@innkv.com>
2024-07-24 17:03:45 -0400

    Final
    :(

--- bookEqx/webapp/admin.py
+++ bookEqx/webapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Room
+
+admin.site.register(Room)
+
+
+from .models import Reservation
+
+admin.site.register(Reservation)
